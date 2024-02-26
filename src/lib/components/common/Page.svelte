ibpl/web-certificate-tool

commit d5b75e9f058781980c0e73299455ddf44ad1a908

Remove unused parameters
Author-Change-Id: IB#1141363
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

src/lib/components/common/Page.svelte
@@ -7,12 +7,6 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 	import { t } from '$lib/i18n';
 	import ErrorBanner from '$lib/components/common/ErrorBanner.svelte';
 
-	// title is string displayed in bar.
-	export let title = '';
-
-	// contentEnabled allows page content area padding customization.
-	export let contentPadding = '0rem 1rem 2rem 1rem';
-
 	// contentEnabled allows disabling access to all controls on page content when set to false.
 	export let contentEnabled = true;
 
@@ -21,11 +15,11 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 </script>
 
 <svelte:head>
-	<title>{title ? title + ' • ' : ''}{$t('common.webCertificateTool')}</title>
+	<title>{$t('common.webCertificateTool')}</title>
 </svelte:head>
 
 <ErrorBanner bind:message={errorBanner} />
-<div class="page-content{contentEnabled ? '' : ' disabled'}" style="padding: {contentPadding};">
+<div class="page-content{contentEnabled ? '' : ' disabled'}" style="padding: 0rem 1rem 2rem 1rem;">
 	<slot />
 </div>
 

commit 6e1d62214b323a267f40f3b85d85bf51ffa56b67

Restore required parameter
Fixes: d5b75e9f058781980c0e73299455ddf44ad1a908
Author-Change-Id: IB#1141363
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

src/lib/components/common/Page.svelte
@@ -7,6 +7,8 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 	import { t } from '$lib/i18n';
 	import ErrorBanner from '$lib/components/common/ErrorBanner.svelte';
 
+	// title is string displayed in bar.
+	export let title = '';
 	// contentEnabled allows disabling access to all controls on page content when set to false.
 	export let contentEnabled = true;
 
@@ -15,7 +17,7 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 </script>
 
 <svelte:head>
-	<title>{$t('common.webCertificateTool')}</title>
+	<title>{title ? title + ' • ' : ''}{$t('common.webCertificateTool')}</title>
 </svelte:head>
 
 <ErrorBanner bind:message={errorBanner} />