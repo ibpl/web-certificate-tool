ibpl/web-certificate-tool

commit f2c395efea4ecc48b7797e9c330f64bab81ebce2

Add unit tests, update dependencies and cleanup
Add unit tests, update dependencies and remove unused component.

Author-Change-Id: IB#1141363
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

src/lib/components/common/Page.svelte
@@ -5,7 +5,6 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 
 <script lang="ts">
 	import { t, locale, locales } from '$lib/i18n';
-	import ErrorBanner from '$lib/components/common/ErrorBanner.svelte';
 	import Button, { Icon, Label } from '@smui/button';
 	import Menu from '@smui/menu';
 	import List, { Item, Text } from '@smui/list';
@@ -23,9 +22,6 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 
 	// contentEnabled allows disabling access to all controls on page content when set to false.
 	export let contentEnabled = true;
-
-	// errorBanner is error message to be displayed in banner.
-	export let errorBanner: string | undefined = undefined;
 
 	// themeModes contains all avalable theme mode selections.
 	const themeModes = [
@@ -61,26 +57,31 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 </script>
 
 <svelte:head>
+	<!-- /* v8 ignore next */ -->
 	<title>{title ? title + ' • ' : ''}{$t('common.webCertificateTool')}</title>
 </svelte:head>
 
-<!-- Display error banner if errorBanner is not empty. -->
-<ErrorBanner bind:message={errorBanner} />
-
 <!-- Greyout page content if disabled. -->
+<!-- /* v8 ignore next */ -->
 <div class="page-content{contentEnabled ? '' : ' disabled'}" style="padding: 0rem 1rem 2rem 1rem;">
 	<div
 		style="display: flex; justify-content: flex-end; align-items: baseline; flex-wrap: wrap-reverse;"
 	>
 		<!-- Page title to be displayed on the left in the top bar row. -->
-		<div style="flex-grow: 1;"><h6>{title ? title : $t('common.webCertificateTool')}</h6></div>
+		<div style="flex-grow: 1;">
+			<!-- /* v8 ignore next */ -->
+			<h6 data-testid="page-title">{title ? title : $t('common.webCertificateTool')}</h6>
+		</div>
 		<!-- Menu buttons to be displayed on the right in the top bar row. Wraps as the first on small screens. -->
 		<div style="display: flex; justify-content: flex-end; align-items: baseline; margin-top: 1rem;">
 			<!-- Language change button and menu. -->
 			<div class="menu-button">
 				<Button
+					data-testid="button-language"
 					title={$t('common.changeLanguage')}
-					on:click={() => menuLanguage.setOpen(true)}
+					on:click={() => {
+						$locale = 'pl';
+					}}
 					variant="outlined"
 					style="padding: 0; min-width: 70px;"
 				>
@@ -96,10 +97,12 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 					<List>
 						{#each $locales as l}
 							<Item
+								data-testid="menu-language-{l}"
 								on:SMUI:action={() => {
 									$locale = l;
 								}}
 							>
+								<!-- /* v8 ignore next */ -->
 								<Text>{$t(`lang.${l}`)}</Text>
 							</Item>
 						{/each}
@@ -109,13 +112,18 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 			<!-- Theme mode change button and menu. -->
 			<div class="menu-button">
 				<Button
+					data-testid="button-theme-mode"
 					title={$t('common.changeThemeMode')}
 					on:click={() => menuThemeMode.setOpen(true)}
 					variant="outlined"
 					style="padding: 0; min-width: 60px;"
 				>
 					<Icon tag="svg" style="width: 1em; height: auto; margin: 0;" viewBox="0 0 24 24">
-						<path fill="currentColor" d={selectedThemeMode.icon} />
+						<path
+							data-testid="button-theme-mode-icon-path"
+							fill="currentColor"
+							d={selectedThemeMode.icon}
+						/>
 					</Icon>
 					<Icon tag="svg" style="width: 1em; height: auto; margin: 0;" viewBox="0 0 24 24">
 						<path fill="currentColor" d={mdiMenuDown} />
@@ -124,7 +132,10 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 				<Menu bind:this={menuThemeMode} anchorCorner="BOTTOM_START">
 					<List>
 						{#each themeModes as themeMode}
-							<Item on:SMUI:action={() => (selectedThemeMode = themeMode)}>
+							<Item
+								data-testid="menu-theme-mode-{themeMode.id}"
+								on:SMUI:action={() => (selectedThemeMode = themeMode)}
+							>
 								<Icon
 									tag="svg"
 									style="width: 1em; height: auto; margin: 0px 5px 0px 0px"

src/lib/components/common/ErrorBanner.svelte
@@ -1,65 +1,0 @@
-<!--
-SPDX-License-Identifier: AGPL-3.0-only
-SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://www.ib.pl>
--->
-
-<script lang="ts">
-	import { t } from '$lib/i18n';
-	import Button from '@smui/button';
-	import Banner, { Label, Icon, CloseReason } from '@smui/banner';
-	import { mdiAlertCircle } from '@mdi/js';
-
-	export let message: string | undefined = undefined; // message is error message displayed in error banner.
-
-	let banner: Banner; // banner is error banner handle.
-	let open = false; // open is banner opening status.
-	let content: string | undefined = undefined; // content is content displayed in banners.
-
-	// messageChangeHandler updates banner state on message changes.
-	function messageChangeHandler(message: string | undefined) {
-		if (message !== undefined) {
-			content = message;
-			if (!open) {
-				open = true;
-			}
-			// https://github.com/hperrin/svelte-material-ui/issues/577 workaround.
-			setTimeout(() => {
-				banner.layout();
-			}, 0);
-		} else {
-			if (open) {
-				open = false;
-			}
-		}
-	}
-	$: messageChangeHandler(message);
-
-	// closeHandler handles error banner close.
-	function closeHandler(event: CustomEvent<{ reason: CloseReason }>) {
-		switch (event.detail.reason) {
-			case CloseReason.PRIMARY:
-				message = undefined;
-				break;
-		}
-	}
-</script>
-
-<Banner
-	bind:this={banner}
-	bind:open
-	mobileStacked={true}
-	on:SMUIBanner:closed={closeHandler}
-	on:SMUIBanner:opened={() => {
-		// https://github.com/hperrin/svelte-material-ui/issues/577 workaround.
-		banner.layout();
-	}}
->
-	<Icon slot="icon" tag="svg" viewBox="0 0 24 24">
-		<path fill="currentColor" d={mdiAlertCircle} />
-	</Icon>
-	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-	<Label slot="label">{@html content}</Label>
-	<svelte:fragment slot="actions">
-		<Button>{$t('common.ok')}</Button>
-	</svelte:fragment>
-</Banner>

src/lib/components/common/Page.test.ts
@@ -2,20 +2,77 @@
 // SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://www.ib.pl>
 
 import Page from './Page.svelte';
-import { render } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
+import { mdiWhiteBalanceSunny, mdiWeatherNight, mdiThemeLightDark } from '@mdi/js';
 
 describe('Page', () => {
-	// Render the Page component and check if it's present.
-	test('rendering', async () => {
-		const { container } = render(Page, { props: { contentEnabled: true, title: 'title123' } });
+	test('page with enabled content is rendered correctly', () => {
+		const { container } = render(Page, { props: { contentEnabled: true } });
+
 		const div = container.querySelector('.disabled');
 		expect(div).toBeFalsy();
 	});
 
-	// Hide the Page component and check if it's hidden.
-	test('hiding', async () => {
+	test('page with disabled content is rendered correctly', () => {
 		const { container } = render(Page, { props: { contentEnabled: false } });
 		const div = container.querySelector('.disabled');
 		expect(div).toBeTruthy();
+	});
+
+	test('default page title works', () => {
+		render(Page, { props: { contentEnabled: false, title: '' } });
+		const title = screen.getByTestId('page-title');
+		expect(title.innerHTML).toBe('Web Certificate Tool');
+	});
+
+	test('custom page title works', () => {
+		render(Page, { props: { contentEnabled: false, title: 'title123' } });
+		const title = screen.getByTestId('page-title');
+		expect(title.innerHTML).toBe('title123');
+	});
+
+	test('language changing works', async () => {
+		const user = userEvent.setup();
+		render(Page, { props: { contentEnabled: true } });
+		const title = screen.getByTestId('page-title');
+
+		// Default (EN)
+		expect(title.innerHTML).toBe('Web Certificate Tool');
+		const buttonLanguage = screen.getByTestId('button-language');
+
+		// PL
+		await user.click(buttonLanguage);
+		const menuLanguagePL = screen.getByTestId('menu-language-pl');
+		await user.click(menuLanguagePL);
+		expect(title.innerHTML).toBe('Webowe narzędzie do certyfikatów');
+
+		// EN
+		await user.click(buttonLanguage);
+		const menuLanguageEN = screen.getByTestId('menu-language-en');
+		await user.click(menuLanguageEN);
+		expect(title.innerHTML).toBe('Web Certificate Tool');
+	});
+
+	test('theme mode changing works', async () => {
+		const user = userEvent.setup();
+		render(Page, { props: { contentEnabled: true } });
+		const buttonThemeMode = screen.getByTestId('button-theme-mode');
+		const buttonThemeModeIconPath = screen.getByTestId('button-theme-mode-icon-path');
+
+		// Light theme mode.
+		await user.click(buttonThemeMode);
+		await user.click(screen.getByTestId('menu-theme-mode-1'));
+		expect(buttonThemeModeIconPath.getAttribute('d')).toBe(mdiWhiteBalanceSunny);
+
+		// System theme mode.
+		await user.click(buttonThemeMode);
+		await user.click(screen.getByTestId('menu-theme-mode-2'));
+		expect(buttonThemeModeIconPath.getAttribute('d')).toBe(mdiThemeLightDark);
+
+		// Dark theme mode.
+		await user.click(buttonThemeMode);
+		await user.click(screen.getByTestId('menu-theme-mode-3'));
+		expect(buttonThemeModeIconPath.getAttribute('d')).toBe(mdiWeatherNight);
 	});
 });

commit f362505b7e33c9c0b1b4ceb317f685ff4c48e714

Fix language button actions
Restore valid language button onclick handler.

Fixes: f2c395efea4ecc48b7797e9c330f64bab81ebce2
Author-Change-Id: IB#1141363
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

src/lib/components/common/Page.svelte
@@ -79,9 +79,7 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 				<Button
 					data-testid="button-language"
 					title={$t('common.changeLanguage')}
-					on:click={() => {
-						$locale = 'pl';
-					}}
+					on:click={() => menuLanguage.setOpen(true)}
 					variant="outlined"
 					style="padding: 0; min-width: 70px;"
 				>