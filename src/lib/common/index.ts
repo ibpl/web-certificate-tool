ibpl/web-certificate-tool

commit 534053caf10fb56a44565c40c7a881a967bec013

Add check for SubtleCrypto in browser
Author-Change-Id: IB#1141363
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

src/lib/common/index.ts
@@ -45,6 +45,22 @@ export function isValidConfig(arg: unknown): boolean {
 	return true;
 }
 
+// checkCryptoRequirements check for required crypto API stuff and throws an error if
+// browser does not meet requirements.
+export function checkCryptoRequirements() {
+	// SubtleCrypto interface of the Web Crypto API must be available.
+	if (typeof crypto === 'undefined' || !('subtle' in crypto)) {
+		throw <App.Error>{
+			status: 418,
+			message: t.get('common.unsupportedBrowser'),
+			details: t.get('common.requirementNotAvailable', {
+				requirement:
+					'<a href="https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto#browser_compatibility">SubtleCrypto</a>'
+			})
+		};
+	}
+}
+
 // tsFetch is generic fetch wrapper for fetching typed data.
 async function tsFetch<T>(url: string, options = {}): Promise<T> {
 	let response: Response;
@@ -228,6 +244,7 @@ export async function initializeEnvironment(url: URL) {
 	if (initializeEnvironmentInternalPaths.get(url.pathname) === undefined) {
 		initializeEnvironmentInternalPaths.set(url.pathname, initializeEnvironmentInternal(url));
 	}
+
 	return initializeEnvironmentInternalPaths.get(url.pathname);
 }
 

src/routes/+page.ts
@@ -2,7 +2,7 @@
 // SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://www.ib.pl>
 
 import type { PageLoad } from './$types';
-import { initializeEnvironment } from '$lib/common';
+import { initializeEnvironment, checkCryptoRequirements } from '$lib/common';
 
 export const load = (async ({ url }) => {
 	// Note: initializeEnvironment must be executed on top of load function
@@ -11,6 +11,9 @@ export const load = (async ({ url }) => {
 	// by this function are ignored in load() in root +layout.ts but should
 	// not be ignored in other places.
 	await initializeEnvironment(url);
+
+	// Check for required crypto API stuff and throw an error if browser does not meet requirements.
+	checkCryptoRequirements();
 
 	return {};
 }) satisfies PageLoad;

src/lib/common/index.test.ts
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://www.ib.pl>
 
 import {
+	checkCryptoRequirements,
 	initializeEnvironment,
 	resetEnvironment,
 	isValidConfig,
@@ -24,6 +25,18 @@ describe('isValidConfig', () => {
 		expect(isValidConfig(1)).toBeFalsy();
 		expect(isValidConfig({ darkTheme: 1, locale: 'en' })).toBeFalsy();
 		expect(isValidConfig({ darkTheme: false, locale: 0 })).toBeFalsy();
+	});
+});
+
+// Requirements check.
+describe('checkCryptoRequirements', () => {
+	test('should throw error when subtle is not available in crypto', () => {
+		vi.stubGlobal('crypto', { dummy: 'test' });
+		expect(() => checkCryptoRequirements()).toThrowError('Unsupported browser.');
+	});
+	test('should throw error when crypto is undefined', () => {
+		vi.stubGlobal('crypto', undefined);
+		expect(() => checkCryptoRequirements()).toThrowError('Unsupported browser.');
 	});
 });
 

src/routes/+error.svelte
@@ -25,7 +25,8 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 		</h6>
 		<p>
 			{#if $page?.error?.operation}{$t('common.operation')}: {$page.error.operation}<br />{/if}
-			{#if $page?.error?.details}{$t('common.details')}: {$page.error.details}<br />{/if}
+			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+			{#if $page?.error?.details}{$t('common.details')}: {@html $page.error.details}<br />{/if}
 			{#if $page?.error?.url}{$t('common.url')}: {$page.error.url}<br />{/if}
 		</p>
 	</Page>

commit 8157a4bd6b70d59db704d7373cc332335ca50ebf

Rename function
Rename function to more general for future enhancemets.

Fixes: 534053caf10fb56a44565c40c7a881a967bec013
Author-Change-Id: IB#1141363
Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

src/lib/common/index.ts
@@ -45,9 +45,8 @@ export function isValidConfig(arg: unknown): boolean {
 	return true;
 }
 
-// checkCryptoRequirements check for required crypto API stuff and throws an error if
-// browser does not meet requirements.
-export function checkCryptoRequirements() {
+// checkBrowserCompatibility checks whether browser meets requirements and throws an error if not.
+export function checkBrowserCompatibility() {
 	// SubtleCrypto interface of the Web Crypto API must be available.
 	if (typeof crypto === 'undefined' || !('subtle' in crypto)) {
 		throw <App.Error>{

src/routes/+page.ts
@@ -2,7 +2,7 @@
 // SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://www.ib.pl>
 
 import type { PageLoad } from './$types';
-import { initializeEnvironment, checkCryptoRequirements } from '$lib/common';
+import { initializeEnvironment, checkBrowserCompatibility } from '$lib/common';
 
 export const load = (async ({ url }) => {
 	// Note: initializeEnvironment must be executed on top of load function
@@ -12,8 +12,8 @@ export const load = (async ({ url }) => {
 	// not be ignored in other places.
 	await initializeEnvironment(url);
 
-	// Check for required crypto API stuff and throw an error if browser does not meet requirements.
-	checkCryptoRequirements();
+	// Check for browser compatibility and throw an error if browser does not meet requirements.
+	checkBrowserCompatibility();
 
 	return {};
 }) satisfies PageLoad;

src/lib/common/index.test.ts
@@ -2,7 +2,7 @@
 // SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://www.ib.pl>
 
 import {
-	checkCryptoRequirements,
+	checkBrowserCompatibility,
 	initializeEnvironment,
 	resetEnvironment,
 	isValidConfig,
@@ -28,15 +28,15 @@ describe('isValidConfig', () => {
 	});
 });
 
-// Requirements check.
-describe('checkCryptoRequirements', () => {
+// Browser compatibility verification.
+describe('checkBrowserCompatibility', () => {
 	test('should throw error when subtle is not available in crypto', () => {
 		vi.stubGlobal('crypto', { dummy: 'test' });
-		expect(() => checkCryptoRequirements()).toThrowError('Unsupported browser.');
+		expect(() => checkBrowserCompatibility()).toThrowError('Unsupported browser.');
 	});
 	test('should throw error when crypto is undefined', () => {
 		vi.stubGlobal('crypto', undefined);
-		expect(() => checkCryptoRequirements()).toThrowError('Unsupported browser.');
+		expect(() => checkBrowserCompatibility()).toThrowError('Unsupported browser.');
 	});
 });
 