f4cea70e8495cac1900ed8a5166313cab87fbe3a
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2024-03-12 12:51:29 +0100

    Hint component added
    Author-Change-Id: IB#1141363
    Signed-off-by: Pawel Boguslawski <pawel.boguslawski@ib.pl>

---
+++ src/lib/components/common/Hint.svelte
@@ -0,0 +1,53 @@
+<!--
+SPDX-License-Identifier: AGPL-3.0-only
+SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://www.ib.pl>
+-->
+
+<script lang="ts">
+	import { Icon } from '@smui/common';
+	import Paper, { Title, Subtitle, Content } from '@smui/paper';
+	import { mdiLightbulbOnOutline } from '@mdi/js';
+</script>
+
+<div class="hint-container">
+	<Paper class="hint-paper">
+		<div class="hint-icon smui-paper.smui-paper--raised">
+			<Icon tag="svg" viewBox="0 0 24 24">
+				<path fill="currentColor" d={mdiLightbulbOnOutline} />
+			</Icon>
+		</div>
+		<div class="hint-text">
+			<Title><slot name="title" /></Title>
+			<Subtitle><slot name="subtitle" /></Subtitle>
+			<Content><slot name="content" /></Content>
+		</div>
+	</Paper>
+</div>
+
+<style>
+	.hint-container {
+		display: flex;
+		justify-content: right;
+		margin-bottom: 1rem;
+	}
+
+	* :global(.hint-paper) {
+		flex: 0 1 700px;
+		display: flex;
+		opacity: 0.5;
+		padding: 0;
+	}
+
+	.hint-icon {
+		flex: 0 0 24px;
+		display: flex;
+		flex-direction: column;
+		padding: 24px 16px;
+		border-radius: var(--mdc-shape-medium, 4px) 0 0 var(--mdc-shape-medium, 4px);
+	}
+
+	.hint-text {
+		flex-grow: 4;
+		padding: 24px 16px;
+	}
+</style>
