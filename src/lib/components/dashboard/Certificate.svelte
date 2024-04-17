ibpl/web-certificate-tool

commit 373303666d2a6ce8c00d411892682cf375426ea6

Fix uploading same file multiple times
Related: https://stackoverflow.com/questions/4109276/how-to-detect-input-type-file-change-for-the-same-file
Author-Change-Id: IB#1141363

src/lib/components/dashboard/Certificate.svelte
@@ -148,6 +148,9 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 				t.get('dashboard.errorLoadingCrtFromFile') +
 				'.';
 		}
+		// Reset file input value to workaround problems with uploading same file again in some browsers.
+		// See https://stackoverflow.com/questions/4109276/how-to-detect-input-type-file-change-for-the-same-file
+		crtFileInput.value = '';
 	}
 
 	// generatePKCS12 generates PKCS #12 archive with private key and certificate and download it.

src/lib/components/dashboard/Key.svelte
@@ -175,6 +175,9 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 				t.get('dashboard.errorLoadingKeyFromFile') +
 				'.';
 		}
+		// Reset file input value to workaround problems with uploading same file again in some browsers.
+		// See https://stackoverflow.com/questions/4109276/how-to-detect-input-type-file-change-for-the-same-file
+		keyFileInput.value = '';
 	}
 </script>
 

commit cdf24185ae382366bc7145bd06c0a81d63777a02

Fix tests
Fixes: 373303666d2a6ce8c00d411892682cf375426ea6
Author-Change-Id: IB#1234567

src/lib/components/dashboard/Certificate.svelte
@@ -150,7 +150,9 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 		}
 		// Reset file input value to workaround problems with uploading same file again in some browsers.
 		// See https://stackoverflow.com/questions/4109276/how-to-detect-input-type-file-change-for-the-same-file
-		crtFileInput.value = '';
+		if (crtFileInput && crtFileInput.value) {
+			crtFileInput.value = '';
+		}
 	}
 
 	// generatePKCS12 generates PKCS #12 archive with private key and certificate and download it.

src/lib/components/dashboard/Key.svelte
@@ -177,7 +177,9 @@ SPDX-FileCopyrightText: 2024 Informatyka Boguslawski sp. z o.o. sp.k. <https://w
 		}
 		// Reset file input value to workaround problems with uploading same file again in some browsers.
 		// See https://stackoverflow.com/questions/4109276/how-to-detect-input-type-file-change-for-the-same-file
-		keyFileInput.value = '';
+		if (keyFileInput && keyFileInput.value) {
+			keyFileInput.value = '';
+		}
 	}
 </script>
 

src/lib/components/dashboard/Certificate.test.ts
@@ -95,12 +95,6 @@ async function testCrtUploadFailure(crtPem: string, ownerId: string, errorMessag
 
 	const file = new File([crtPem], 'test.crt', { type: 'application/x-pem-file' });
 	await user.upload(input, file);
-	expect(input.files).toBeTruthy();
-	if (input.files) {
-		expect(input.files[0]).toStrictEqual(file);
-		expect(input.files.item(0)).toStrictEqual(file);
-		expect(input.files).toHaveLength(1);
-	}
 
 	// Error should be displayed.
 	await vi.waitFor(
@@ -132,12 +126,6 @@ async function testCrtUploadSuccess(
 
 	const file = new File([crtPem], 'test.crt', { type: 'application/x-pem-file' });
 	await user.upload(input, file);
-	expect(input.files).toBeTruthy();
-	if (input.files) {
-		expect(input.files[0]).toStrictEqual(file);
-		expect(input.files.item(0)).toStrictEqual(file);
-		expect(input.files).toHaveLength(1);
-	}
 
 	// Check loaded certificate information.
 	await vi.waitFor(

src/lib/components/dashboard/DashboardPage.test.ts
@@ -149,12 +149,7 @@ describe('DashboardPage', () => {
 		const input = <HTMLInputElement>screen.getByTestId('input-load-key');
 		const file = new File([testRSA2048KeyPEM], 'test.key', { type: 'application/x-pem-file' });
 		await user.upload(input, file);
-		expect(input.files).toBeTruthy();
-		if (input.files) {
-			expect(input.files[0]).toStrictEqual(file);
-			expect(input.files.item(0)).toStrictEqual(file);
-			expect(input.files).toHaveLength(1);
-		}
+
 		// No error should be set.
 		await vi.waitFor(() => expect(get(errorDialogMessage)).toBe(''), { timeout: 3000 });
 
@@ -198,12 +193,6 @@ describe('DashboardPage', () => {
 		const inputCrt = <HTMLInputElement>screen.getByTestId('input-load-crt');
 		const fileCrt = new File([testCrtPEM], 'test.crt', { type: 'application/x-pem-file' });
 		await user.upload(inputCrt, fileCrt);
-		expect(inputCrt.files).toBeTruthy();
-		if (inputCrt.files) {
-			expect(inputCrt.files[0]).toStrictEqual(fileCrt);
-			expect(inputCrt.files.item(0)).toStrictEqual(fileCrt);
-			expect(inputCrt.files).toHaveLength(1);
-		}
 		await vi.waitFor(() =>
 			expect(buttonGenerateAndDownloadP12.getAttribute('disabled')).toBe(null)
 		);

src/lib/components/dashboard/Key.test.ts
@@ -814,12 +814,6 @@ async function testKeyUploadFailure(key: string, password: string, errorMessage:
 
 	const file = new File([key], 'test.key', { type: 'application/x-pem-file' });
 	await user.upload(input, file);
-	expect(input.files).toBeTruthy();
-	if (input.files) {
-		expect(input.files[0]).toStrictEqual(file);
-		expect(input.files.item(0)).toStrictEqual(file);
-		expect(input.files).toHaveLength(1);
-	}
 
 	// Error should be set.
 	await vi.waitFor(() => expect(get(errorDialogMessage)).toBe(errorMessage));
@@ -844,12 +838,6 @@ async function testKeyUploadSuccess(
 
 	const file = new File([key], 'test.key', { type: 'application/x-pem-file' });
 	await user.upload(input, file);
-	expect(input.files).toBeTruthy();
-	if (input.files) {
-		expect(input.files[0]).toStrictEqual(file);
-		expect(input.files.item(0)).toStrictEqual(file);
-		expect(input.files).toHaveLength(1);
-	}
 
 	// No error should be set.
 	await vi.waitFor(() => expect(get(errorDialogMessage)).toBe(''), { timeout: 3000 });